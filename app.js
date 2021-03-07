384bd8b0d1654f6526576fdbfeb1d7b77fc85a70
milad <miladxgh@outlook.com>
2021-03-07 19:45:40 +0330

    play/pause button works

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// dom elements
+const progress = document.getElementById('progress');
+const playBtn = document.getElementById('play');
 
+let isPlaying = false;
+let duration = 0;
+let currentTime = 0;
+
+let song = new Audio('music/01. Algorithm.mp3');
+
+function playSong() {
+    if (!isPlaying) {
+        song.play();
+        isPlaying = true;
+    } else {
+        song.pause();
+        isPlaying = false;
+    }
+}
+
+playBtn.addEventListener("click", playSong);
