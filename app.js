f2a5c5eab2afe2861fd00595ab3732ee528eadb8
milad <miladxgh@outlook.com>
2021-03-07 20:25:25 +0330

    slider functions better

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ let isPlaying = false;
 let duration = 0;
 let currentTime = 0;
 
+let playList = [
+    
+];
 let song = new Audio('music/01. Algorithm.mp3');
 
 function playSong() {
     if (!isPlaying) {
         song.play();
-        
         isPlaying = true;
         duration = song.duration;
         progress.max = duration;
@@ -28,11 +30,31 @@ function playSong() {
 
 playBtn.addEventListener("click", playSong);
 
+
+
+let touch = false;
+progress.addEventListener("mousedown", function() {
+    touch = true;
+    console.log(touch);
+});
+progress.addEventListener("mouseup", function() {
+    touch = false;
+    console.log(touch);
+});
 progress.addEventListener("change", function() {
     song.currentTime = progress.value;
+    
 });
-
 song.addEventListener("timeupdate", function() {
-    progress.value = song.currentTime;
+    if (touch == false) {
+        progress.value = song.currentTime;
+    }
+});
+song.addEventListener("ended", function() {
+    song.pause();
+    isPlaying = false;
+    song.currentTime = 0;
+    progress.value = 0;
+    playIcon.className = "fa fa-play";
 });
 
