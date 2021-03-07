7497dc33e2dff2bb7c644d04424bfe0ac474c6c9
milad <miladxgh@outlook.com>
2021-03-07 23:05:53 +0330

    play playlist

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 // dom elements
+const cover = document.getElementById('cover');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+
 const progress = document.getElementById('progress');
 const playBtn = document.getElementById('play');
 const playIcon = document.getElementById('play-icon');
+const nextBtn = document.getElementById('next');
 
 let isPlaying = false;
 let duration = 0;
 let currentTime = 0;
 
 let playList = [
-    
+    {title: 'The Dark side', artist: 'Muse', album:'Simulation Theory', src: 'music/The Dark Side.mp3', cover: 'cover/simulation-theory.jpg'},
+    {title: 'Born to Die', artist: 'Lana Del Rey', album:'Born to Die', src: 'music/Born to Die.mp3', cover: 'cover/born-to-die.jpg'},
 ];
-let song = new Audio('music/01. Algorithm.mp3');
+let track = 0;
+let nowPlaying = playList[track];
+console.log(nowPlaying);
+
+let song = new Audio('music/The Dark Side.mp3');
+
+
 
 function playSong() {
+    song.src = nowPlaying.src;
+    // update music info
+    cover.src = nowPlaying.cover;
+    title.innerHTML = nowPlaying.title;
+    artist.innerHTML = nowPlaying.artist;
+   
+
     if (!isPlaying) {
         song.play();
         isPlaying = true;
         duration = song.duration;
+         console.log(song.duration);
         progress.max = duration;
         playIcon.className = "fa fa-pause";
-        
     } else {
         song.pause();
         isPlaying = false;
@@ -28,25 +47,43 @@ function playSong() {
     }
 }
 
-playBtn.addEventListener("click", playSong);
+// function nextSong() {
+//     track = 1;
+//     song.play();
+//     cover.src = nowPlaying.cover;
+//     title.innerHTML = nowPlaying.title;
+//     artist.innerHTML = nowPlaying.artist;
+// }
 
+
+playBtn.addEventListener("click", playSong);
+nextBtn.addEventListener("click", () => {
+    if (track == 0) {
+        track = 1;
+    } else {
+        track = 0;
+    }
+    nowPlaying = playList[track];
+    isPlaying = false;
+    playSong();
+});
 
 
 let touch = false;
 progress.addEventListener("mousedown", function() {
     touch = true;
-    console.log(touch);
+    
 });
 progress.addEventListener("mouseup", function() {
     touch = false;
-    console.log(touch);
+    
 });
 progress.addEventListener("change", function() {
     song.currentTime = progress.value;
     
 });
 song.addEventListener("timeupdate", function() {
-    if (touch == false) {
+    if (touch == false) { // if user not clicking on [input:range] match song current time to [input:range] 
         progress.value = song.currentTime;
     }
 });
