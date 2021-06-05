7599af8bec9b70df9f6149bdfb77aa36f07220bd
Milad <miladxgh@outlook.com>
2021-06-05 20:25:34 +0430

    settings component

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ let duration = 0;
 let currentTime = 0;
 
 let playList = [
-    {title: 'Time',            artist: 'Alan Walker & Hans Zimmer', src: 'music/Time (Alan Walker Remix).m4a', cover: 'cover/Time.jpg'},
-    {title: 'In The End',      artist: 'Fleurie',                   src: 'music/In The End (remix).mp3',       cover: 'cover/In The End.jpg'},
-    {title: 'Algorithm',       artist: 'Muse',                      src: 'music/01. Algorithm.mp3',            cover: 'cover/Simulation.Theory.jpg'},
-    {title: 'Never Fade Away', artist: 'Olga Jankowska',            src: 'music/Never Fade Away.mp3',          cover: 'cover/Cyberpunk.jpg'}
+    { title: 'Time', artist: 'Alan Walker & Hans Zimmer', src: 'music/Time (Alan Walker Remix).m4a', cover: 'cover/Time.jpg' },
+    { title: 'In The End', artist: 'Fleurie', src: 'music/In The End (remix).mp3', cover: 'cover/In The End.jpg' },
+    { title: 'Algorithm', artist: 'Muse', src: 'music/01. Algorithm.mp3', cover: 'cover/Simulation.Theory.jpg' },
+    { title: 'Never Fade Away', artist: 'Olga Jankowska', src: 'music/Never Fade Away.mp3', cover: 'cover/Cyberpunk.jpg' }
 ];
 
 let track = 0;
@@ -33,14 +33,14 @@ song.volume = 0.5;
 
 
 // change volume
-volume.addEventListener("input", function() {
-     song.volume = volume.value/100;
+volume.addEventListener("input", function () {
+    song.volume = volume.value / 100;
 });
 
 song.src = nowPlaying.src;
 let currentSrc = nowPlaying.src;
 
-song.onloadeddata = function() {
+song.onloadeddata = function () {
     duration = song.duration;
     slider.max = duration;
     // console.log(duration);
@@ -49,28 +49,28 @@ song.onloadeddata = function() {
 
 // functions
 function nowPlayingBorder() {
-    // make border for now playing song when click on menu
+    // SHOW border for now playing song when click on menu
     const trackLists = document.querySelectorAll(".track-list");
     for (let i = 0; i < trackLists.length; i++) {
-        trackLists[i].style.boxShadow = "0 0 2px 2px #00000008"; 
+        trackLists[i].style.boxShadow = "0 0 2px 2px #00000008";
     }
-    trackLists[track].style.boxShadow = "0 0 2px 2px #5a86bf79";    
+    trackLists[track].style.boxShadow = "0 0 2px 2px #5a86bf79";
 }
 
 
 
 function playSong() {
 
-    if(currentSrc != nowPlaying.src) {
+    if (currentSrc != nowPlaying.src) {
         // change song.src only if track has changed
         song.src = nowPlaying.src;
         currentSrc = nowPlaying.src;
-        
+
         document.querySelector('#cover').classList.remove('effect');
         void document.querySelector('#cover').offsetWidth;
         document.querySelector('#cover').classList.add('effect');
     }
-    
+
     nowPlayingBorder();
 
     if (!isPlaying) {
@@ -95,18 +95,18 @@ function playSong() {
 // Control Buttons
 playBtn.addEventListener("click", playSong);
 
-nextBtn.addEventListener("click", function() {
-    track ++;
-    if (track > playList.length-1) track = 0;
-    
+nextBtn.addEventListener("click", function () {
+    track++;
+    if (track > playList.length - 1) track = 0;
+
     nowPlaying = playList[track];
     isPlaying = false;
     playSong();
 });
 
-backBtn.addEventListener("click", function() {
-    track --;
-    if (track < 0) track = playList.length-1;
+backBtn.addEventListener("click", function () {
+    track--;
+    if (track < 0) track = playList.length - 1;
 
     nowPlaying = playList[track];
     isPlaying = false;
@@ -117,10 +117,10 @@ backBtn.addEventListener("click", function() {
 
 // range slider
 let touch = false;
-slider.addEventListener("mousedown", function() { touch = true; });
-slider.addEventListener("mouseup", function() { touch = false; });
+slider.addEventListener("mousedown", function () { touch = true; });
+slider.addEventListener("mouseup", function () { touch = false; });
 
-slider.addEventListener("change", function() {
+slider.addEventListener("change", function () {
     song.currentTime = slider.value;
     setInterval(() => {
         let x = ((slider.value / slider.max) * 100);
@@ -130,25 +130,25 @@ slider.addEventListener("change", function() {
 
 
 function secondsToMinutes(sec) {
-    return Math.floor(sec / 60).toString().padStart(2, '0') +':'+
-           Math.floor(sec % 60).toString().padStart(2, '0');
+    return Math.floor(sec / 60).toString().padStart(2, '0') + ':' +
+        Math.floor(sec % 60).toString().padStart(2, '0');
 }
 
 
 // song events
-song.addEventListener("timeupdate", function() {
+song.addEventListener("timeupdate", function () {
     if (!touch) { // if user not clicking on [input:range], match song's current time to [input:range] 
         slider.value = song.currentTime;
         timeLeft.innerHTML = song.currentTime ? secondsToMinutes(song.currentTime) : '00:00';
         timeRemain.innerHTML = song.currentTime ? secondsToMinutes(song.duration - song.currentTime) : '00:00';
     }
-  
+
 });
 
-song.addEventListener("ended", function() {
-    track ++;
-    if (track > playList.length-1) track = 0;
-    
+song.addEventListener("ended", () => {
+    track++;
+    if (track > playList.length - 1) track = 0;
+
     nowPlaying = playList[track];
     isPlaying = false;
     playSong();
@@ -168,9 +168,23 @@ menuBtn.addEventListener("click", () => {
         trackContainer.style.top = '-10%';
         menuBtn.style.color = 'grey';
     }
-    
+
 });
 
+// toggle settings
+let settings_elem = document.querySelector(".settings-container");
+document.querySelector("#settings-btn").
+addEventListener("click", () => {
+    if (settings_elem.style.height == "380px") {
+        // settings_elem.style.visibility = 'hidden';
+         settings_elem.style.opacity = '0';
+        settings_elem.style.height = '0';
+    } else {
+        settings_elem.style.visibility = 'visible';
+        settings_elem.style.opacity = '1';
+        settings_elem.style.height = '380px';
+    }
+});
 
 // play|pause with spacebar key
 document.body.addEventListener("keydown", event => {
